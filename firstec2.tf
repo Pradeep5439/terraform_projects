3284d55df75e181d35cfbefbb2f7eb8f0c1c2e8a
Pradeep5439 <spradeepkumar5439@gmail.com>
2023-05-09 16:52:31 +0530

    modified version 2.0

--- firstec2.tf
+++ firstec2.tf
@@ -1,16 +1,22 @@
+provider "aws" {
+  region     = var.provider_aws.region
+  access_key = var.provider_aws.access_key
+  secret_key = var.provider_aws.secret_key
+}
 
-data "aws_ami" "app_ami"{
-    most_recent = true
-    owners = [ "amazon" ]
+
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners      = ["amazon"]
 
   filter {
-    name = "name"
-    values = [ "amzn2-ami-hvm*" ]
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
   }
 
 }
 
 resource "aws_instance" "firstec2" {
-  ami                     = data.aws_ami.app_ami.id
-  instance_type           = "t2.micro"
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t2.micro"
 }
